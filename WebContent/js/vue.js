874cb918895a2879608dc95516d89ac53bfb3df7
liu.hb <1282072571@qq.com>
2018-08-13 17:42:51 +0800

    报错

--- WebContent/js/vue.js
+++ WebContent/js/vue.js
@@ -3,7 +3,7 @@ var vm = new Vue({
 			data: {
 				site: "菜鸟教程",
 				url: "http://www.runoob.com/vue2/vue-start.html",
-				target:'_blank'
+				target:"_blank",
 				alexa: "10000"
 			},
 			methods: {
